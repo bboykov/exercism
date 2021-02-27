6e7b57e32b248829cdbb8511ef3a7414e6f8bc9d
Boyko Boykov <boyko.boykov@gmail.com>
2021-02-27 10:47:37 +0200

    Add grains solution by myself

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -2,15 +2,29 @@
 
 set -euo pipefail
 
+chess_squares_number=$1
 chess_squares_count="64"
 chess_squares_grains=()
 
+if [[ "${chess_squares_number}" == "total" ]]; then
+  total_sum=0
+elif [[ $((chess_squares_number)) -lt 1 || $((chess_squares_number)) -gt 64 ]]; then
+  echo "Error: invalid input"
+  exit 1
+fi
+
 chess_squares_grains[0]=1
 
-for ((i = 1; i <= chess_squares_count; i++)); do
-  chess_squares_grains[i]=$(chess_squares_grains[i-1] * i)
-
-
+for ((i = 0; i < chess_squares_count; i++)); do
+  next_square="$(echo "${chess_squares_grains[i]} + ${chess_squares_grains[i]}"| bc)"
+  chess_squares_grains[$i+1]="${next_square}"
 done
 
-declare -p chess_squares_grains
+if [[ "${chess_squares_number}" == 'total' ]];then
+  for ((i = 0; i < chess_squares_count; i++)); do
+    total_sum=$(echo $total_sum + "${chess_squares_grains[i]}"|bc)
+  done
+  echo "${total_sum}"
+else
+  echo "${chess_squares_grains[${chess_squares_number}-1]}"
+fi
