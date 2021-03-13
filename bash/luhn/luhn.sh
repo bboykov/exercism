36d97ff380fcb8de38972058c42cf64ab9259944
Boyko Boykov <boyko.boykov@gmail.com>
2021-03-13 10:21:04 +0200

    Cleanup luhn solution

--- bash/luhn/luhn.sh
+++ bash/luhn/luhn.sh
@@ -10,19 +10,15 @@ fi
 
 number=${1// /}
 
-if ((${#number} < 2)) || ! str::is_int "${number}"; then
+if ! str::is_int "${number}" || ((${#number} < 2)); then
   echo "false"
   exit 0
 fi
 
-# echo "${number}"
-
 digits=()
 j=0
 for ((i = ${#number} - 1; i >= 0; i--)); do
-  # echo "${number:${i}:1}"
   if ((j % 2)); then
-    # echo "${number:${i}:1}"
     doubled_digit=$(("${number:${i}:1}" * 2))
     if ((doubled_digit > 9)); then
       doubled_digit=$((doubled_digit - 9))
@@ -34,16 +30,10 @@ for ((i = ${#number} - 1; i >= 0; i--)); do
   ((j += 1))
 done
 
-# declare -p digits
-
 sum=0
 for digit in "${digits[@]}"; do
   sum=$((sum + digit))
 done
-
-# echo "${sum}"
-
-# echo "$((sum % 10))"
 
 if ((sum % 10)); then
   echo "false"
