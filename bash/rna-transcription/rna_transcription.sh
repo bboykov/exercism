99a26b24bcc70423ac65ddea236b8d1b148dd390
Boyko Boykov <boyko.boykov@gmail.com>
2021-03-23 09:23:16 +0200

    Add assosiative array solution to rna transcription exercise

--- bash/rna-transcription/rna_transcription.sh
+++ bash/rna-transcription/rna_transcription.sh
@@ -1,24 +1,60 @@
 #!/usr/bin/env bash
 
-input_string=$1
 
-dna_strand_map="GCTA"
-rna_strand_map="CGAU"
-output_string=""
+using_strings() {
+  local input_string=$1
 
-for (( i=0 ; i < ${#input_string} ; i++ )); do
-  input_char="${input_string:$i:1}"
+  local dna_strand_map="GCTA"
+  local rna_strand_map="CGAU"
+  local output_string=""
 
-  if [[ ! "${dna_strand_map}" == *"${input_char}"* ]]; then
-    echo "Invalid nucleotide detected."
-    exit 1
-  fi
+  for (( i=0 ; i < ${#input_string} ; i++ )); do
+    input_char="${input_string:$i:1}"
 
-  for (( j=0 ; j < ${#dna_strand_map} ; j++)); do
-    if [[ "${input_char}" == "${dna_strand_map:$j:1}" ]]; then
-      output_string+="${rna_strand_map:j:1}"
+    if [[ ! "${dna_strand_map}" == *"${input_char}"* ]]; then
+      echo "Invalid nucleotide detected."
+      exit 1
     fi
-  done
-done
 
-echo "${output_string}"
+    for (( j=0 ; j < ${#dna_strand_map} ; j++)); do
+      if [[ "${input_char}" == "${dna_strand_map:$j:1}" ]]; then
+        output_string+="${rna_strand_map:j:1}"
+      fi
+    done
+  done
+
+  echo "${output_string}"
+
+}
+
+using_assosiative_array(){
+  local input_string=$1
+  local output_string=""
+
+  declare -A strand_map
+
+  strand_map=(
+    [G]="C"
+    [C]="G"
+    [T]="A"
+    [A]="U"
+    )
+
+  for (( i=0 ; i < ${#input_string} ; i++ )); do
+    input_char="${input_string:$i:1}"
+
+    if [[ ! "${!strand_map[*]}" == *"${input_char}"* ]]; then
+      echo "Invalid nucleotide detected."
+      exit 1
+    else
+      output_string+="${strand_map[${input_char}]}"
+    fi
+
+  done
+
+  echo "${output_string}"
+
+}
+
+# using_strings "$@"
+using_assosiative_array "$@"
