5681274e20196b301bba76aa06ee588cce37c35c
Boyko Boykov <boyko.boykov@gmail.com>
2021-03-29 14:12:23 +0300

    Improved phone-number solution

--- bash/phone-number/phone_number.sh
+++ bash/phone-number/phone_number.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-input_number=${1//[^[:digit:]]/}
-
-
-if ((${#input_number} < 10)) || ((${input_number:(-10):1} < 2 )) || ((${input_number:(-7):1} < 2 )); then
+invalid () {
   echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9" >&2
   exit 1
-fi
+}
 
-if ((${#input_number} != 10)); then
-  if ((${#input_number} != 11)) || ((${input_number:(-11):1} != 1)); then
-    echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9" >&2
-    exit 1
-  fi
-  input_number=${input_number:(-10)}
-fi
+main(){
+  (( $# == 1  )) || exit 1
 
-echo "${input_number}"
+  local input_number=${1//[^[:digit:]]/}
+
+  (( ${#input_number} == 11 )) && ((${input_number:(-11):1} == 1)) && input_number=${input_number:(-10)}
+  (( ${#input_number} != 10 )) && invalid
+  ((${input_number:(-10):1} < 2 )) || ((${input_number:(-7):1} < 2 )) && invalid
+
+  echo "${input_number}"
+}
+
+main "$@"
