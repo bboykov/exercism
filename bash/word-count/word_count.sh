0f383c1f954172e132b6624329cb5370bc29a4fd
Boyko Boykov <boyko.boykov@gmail.com>
2021-04-14 12:42:31 +0300

    Add a working solution to the word-count exercise

--- bash/word-count/word_count.sh
+++ bash/word-count/word_count.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+# I coun't solve this one. The only solution that I found it worked with GNU
+# bash, version 5.1.0(1)-release (x86_64-apple-darwin19.6.0):
+# https://exercism.io/tracks/bash/exercises/word-count/solutions/92d0a175e53841a6aadd197cf217a62f
 
 set -euo pipefail
+shopt -s extglob # Enable extended glob
 
 die() {
   echo "$*" >&2
@@ -10,11 +14,8 @@ die() {
 main() {
   (($# == 1)) || die "The script takes only one argument"
 
-  local phrase_string=${1,,}
-  phrase_string=${phrase_string//,/ }
-  phrase_string=$(echo -e ${phrase_string}|tr '\n' ' ')
-  phrase_string=${phrase_string//[[:punct:]]/ }
-
+  local -l phrase_string=${1//\\n/ }
+  phrase_string=${phrase_string//[^\'[:alnum:]]/ }
 
   declare -A word_count_array
 
@@ -24,6 +25,8 @@ main() {
 
   for word in "${phrase_array[@]}"; do
     # echo "${word}" # debug
+    word="${word##+(\')}" # Remove all leading apostrophes. Won't work without extglob
+    word="${word%%+(\')}" # Remove all trailing apostrophes. Won't work without extglob
     word_count_array["${word}"]=$((${word_count_array["${word}"]-0} + 1))
   done
 
